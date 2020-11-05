e2d7168e0ab684f6058c46bb7f5ef1526b69f0e0
Pere Parés Casellas <ppares.casellas@gmail.com>
2020-11-05 16:26:26 +0100

    Angular components - part 2 (directives, pipes, material grid list, material card, material button).

--- angular-confusion/conFusion/src/app/app.module.ts
+++ angular-confusion/conFusion/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
@@ -19,8 +22,11 @@ import { MenuComponent } from './menu/menu.component';
         BrowserModule,
         BrowserAnimationsModule,
         MatToolbarModule,
-        FlexLayoutModule,
-        MatListModule
+        MatListModule,
+        MatGridListModule,
+        MatCardModule,
+        MatButtonModule,
+        FlexLayoutModule
     ],
     providers: [],
     bootstrap: [AppComponent]
