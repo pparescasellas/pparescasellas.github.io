14f2990b369a4b4bb0a41f5a357a32601136c812
Pere Parés Casellas <ppares.casellas@gmail.com>
2020-11-12 11:15:07 +0100

    Second Assignment

--- angular-confusion/conFusion/src/app/about/about.component.ts
+++ angular-confusion/conFusion/src/app/about/about.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
+
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    public leaders: Leader[];
 
-  ngOnInit(): void {
-  }
+    constructor(private leaderService: LeaderService) { }
 
+    ngOnInit(): void {
+        this.leaders = this.leaderService.getLeaders();
+    }
 }
