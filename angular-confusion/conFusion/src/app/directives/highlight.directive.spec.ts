effab62aa960b3fd650dba43824737923a5bff8b
Pere Parés Casellas <ppares.casellas@gmail.com>
2020-11-28 16:13:22 +0100

    Component Testing

--- angular-confusion/conFusion/src/app/directives/highlight.directive.spec.ts
+++ angular-confusion/conFusion/src/app/directives/highlight.directive.spec.ts
@@ -1,8 +1,11 @@
 import { HighlightDirective } from './highlight.directive';
+import { ElementRef, Renderer2 } from '@angular/core';
 
 describe('HighlightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightDirective();
-    expect(directive).toBeTruthy();
-  });
+    it('should create an instance', () => {
+        let a: ElementRef;
+        let b: Renderer2;
+        const directive = new HighlightDirective(a, b);
+        expect(directive).toBeTruthy();
+    });
 });
