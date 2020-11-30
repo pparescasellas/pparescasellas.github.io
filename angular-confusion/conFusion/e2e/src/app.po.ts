cdecb7874ace18b0f385ca46806b12e3625d4218
Pere Parés Casellas <ppares.casellas@gmail.com>
2020-11-30 19:47:58 +0100

    End-to-end testing

--- angular-confusion/conFusion/e2e/src/app.po.ts
+++ angular-confusion/conFusion/e2e/src/app.po.ts
@@ -1,11 +1,19 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
-  }
+    navigateTo(link: string): Promise<unknown> {
+        return browser.get(link) as Promise<unknown>;
+    }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
-  }
+    getParagraphTest(selector: string) {
+        return element(by.css(selector)).getText();
+    }
+
+    getElement(selector: string) {
+        return element(by.css(selector));
+    }
+
+    getAllElements(selector: string) {
+        return element.all(by.css(selector));
+    }
 }
